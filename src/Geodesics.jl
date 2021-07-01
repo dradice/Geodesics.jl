393de27025f932d3618930f7c2cc142104bea658
David Radice <david.radice@psu.edu>
2021-07-01 19:41:48 -0400

    Switched to StaticArrays, improved documentation

--- src/Geodesics.jl
+++ src/Geodesics.jl
@@ -1,34 +1,43 @@
 module Geodesics
 
 using ForwardDiff
-using Tullio
-using LoopVectorization
+using StaticArrays, Tullio, LoopVectorization
 
 export IT, IX, IY, IZ
 export Schwartzschild_metric, KerrSchild_metric
 export metric_deriv, coordinate_basis, make_tetrad, Christoffel_symbols
-export geodesic_equation, geodesic_equation!
+export geodesic_equation
 
 const IT = 1
 const IX = 2
 const IY = 3
 const IZ = 4
 
-"Schwartzschild metric in isotropic Cartesian coordinates"
+"""
+    Schwartzschild_metric(t::Real, x::Real, y::Real, z::Real; M::Real = 1.0)
+
+Computes the Schwartzschild spacetime metric in isotropic Cartesian coordinates [arXiv:0904.4184].
+This function returns the metric as a matrix.
+"""
 function Schwartzschild_metric(t::Real, x::Real, y::Real, z::Real; M::Real = 1.0)
     ρs = M/2
     ρ = sqrt(x^2 + y^2 + z^2)
-    g_dd = zeros(typeof(t), 4, 4)
+    g_dd = @MMatrix zeros(typeof(t), 4, 4)
     @inbounds g_dd[IT,IT] = -((1.0 - ρs/ρ)/(1.0 + ρs/ρ))^2
     for a = IX:IZ
         @inbounds g_dd[a,a] = (1.0 + ρs/ρ)^4
     end
-    return g_dd
+    return SMatrix(g_dd)
 end
 
-"Kerr metric in Cartesian Kerr-Schild coordinates"
+"""
+    KerrSchild_metric(t::Real, x::Real, y::Real, z::Real; a::Real = 0.0, M::Real = 1.0)
+
+Computes the Kerr spacetime metric in Cartesian Kerr-Schild coordinates.
+This function returns the metric as a matrix.
+"""
 function KerrSchild_metric(t::Real, x::Real, y::Real, z::Real; a::Real = 0.0, M::Real = 1.0)
-    η_dd = zeros(typeof(t), 4, 4)
+    η_dd = @MMatrix zeros(typeof(t), 4, 4)
     @inbounds η_dd[IT, IT] = -1
     for a = IX:IZ
         @inbounds η_dd[a,a] = 1
@@ -38,35 +47,45 @@ function KerrSchild_metric(t::Real, x::Real, y::Real, z::Real; a::Real = 0.0, M:
     r = sqrt(-a^2/2 + x^2/2 + y^2/2 + z^2/2 + sqrt(a^4 - 2*a^2*x^2 - 2*a^2*y^2 + 2*a^2*z^2 + x^4 + 2*x^2*y^2 + 2*x^2*z^2 + y^4 + 2*y^2*z^2 + z^4)/2)
     f = 2*M*r^3/(r^4 + a^2*z^2)
     k_d = [1, (r*x + a*y)/(r^2 + a^2), (r*y - a*x)/(r^2 + a^2), z/r]
-    @tullio g_dd[a,b] := η_dd[a,b] + f*k_d[a]*k_d[b]
-    return g_dd
+    g_dd = @MMatrix zeros(typeof(t), 4, 4)
+    @tullio g_dd[a,b] = η_dd[a,b] + f*k_d[a]*k_d[b]
+    return SMatrix(g_dd)
 end
 
 """
+    metric_deriv(G, t::Real, x::Real, y::Real, z::Real)
+
 Computes the derivative of the metric
 
-. G must be a function returning a Matrix{Real,4} and taking t, x, y, z as input
+# Arguments
+- `G` must be a function returning a `Matrix{Real,4}` and taking 4 `Real` as input
+- `t`, `x`, `y`, and `z` are 4 `Real` specifing the spacetime point
 """
 function metric_deriv(G, t::Real, x::Real, y::Real, z::Real)
     f(x_u) = vec(G(x_u...))
     Jac = ForwardDiff.jacobian(f, [t,x,y,z])
-    dg_ddd = zeros(typeof(t), 4, 4, 4)
+    dg_ddd = @MArray zeros(typeof(t), 4, 4, 4)
     for a = IT:IZ
         @inbounds dg_ddd[a,:,:] = reshape(Jac[:,a], 4, 4)
     end
-    return dg_ddd
+    return SArray(dg_ddd)
 end
 
-"Creates a coordinate basis (returned as tupled of 4 vectors)"
+"""
+    coordinate_basis()
+
+Creates a coordinate basis (returned as tupled of 4 vectors)
+"""
 function coordinate_basis()
-    ∂t_u = [1., 0., 0., 0.]
-    ∂x_u = [0., 1., 0., 0.]
-    ∂y_u = [0., 0., 1., 0.]
-    ∂z_u = [0., 0., 0., 1.]
+    ∂t_u = @SVector [1., 0., 0., 0.]
+    ∂x_u = @SVector [0., 1., 0., 0.]
+    ∂y_u = @SVector [0., 0., 1., 0.]
+    ∂z_u = @SVector [0., 0., 0., 1.]
     return (∂t_u, ∂x_u, ∂y_u, ∂z_u)
 end
 
-function project_orthogonal(g_dd::Matrix, v::Vector, basis...)
+"Project a given vector ortogonally to a subspace spanned by a basis"
+function project_orthogonal(g_dd::SMatrix, v::SVector, basis...)
     Pv = copy(v)
     for e in basis
         Pv -= (e'*g_dd*e)*(v'*g_dd*e)*e
@@ -74,7 +93,8 @@ function project_orthogonal(g_dd::Matrix, v::Vector, basis...)
     return Pv
 end
 
-function normalize(g_dd::Matrix, v::Vector)
+"Normalize a given vector"
+function normalize(g_dd::SMatrix, v::SVector)
     v2 = v'*g_dd*v
     if v2 < 0
         return v/sqrt(-v2)
@@ -85,12 +105,15 @@ function normalize(g_dd::Matrix, v::Vector)
 end
 
 """
+    make_tetrad(g_dd::Matrix, u::Vector, e1::Vector, e2::Vector, e3::Vector)
+
 Create a tetrad using the Gram-Schmidt orthonormalization procedure
 
-. g_dd must be a pseudo-Riemannian metric
-. u, e1, e2, e3 must be linearly independent vectors
+# Arguments
+- `g_dd` must be a pseudo-Riemannian metric
+- `u`, `e1`, `e2`, `e3` must be linearly independent vectors
 """
-function make_tetrad(g_dd::Matrix, u::Vector, e1::Vector, e2::Vector, e3::Vector)
+function make_tetrad(g_dd::SMatrix, u::SVector, e1::SVector, e2::SVector, e3::SVector)
     u = normalize(g_dd, u)
 
     e1 = project_orthogonal(g_dd, e1, u)
@@ -106,24 +129,41 @@ function make_tetrad(g_dd::Matrix, u::Vector, e1::Vector, e2::Vector, e3::Vector
 end
 
 """
+    make_tetrad(G, t::Real, x::Real, y::Real, z::Real)
+
 Creates a tetrad using the coordinate basis as building block
 
-. G must be a function returning a Matrix{Real,4} and taking t, x, y, z as input
+# Arguments
+- `G` must be a function returning a `Matrix{Real,4}` and taking 4 `Real` as input
+- `t`, `x`, `y`, and `z` are 4 `Real` specifing the spacetime point
 """
 function make_tetrad(G, t::Real, x::Real, y::Real, z::Real)
     return make_tetrad(G(t, x, y, z), coordinate_basis()...)
 end
 
-"Computes the Christoffel symbols given the metric"
-function Christoffel_symbols(g_uu::Matrix, dg_ddd::Array{T, 3} where T)
-    @tullio Γ_udd[a,b,c] := 0.5*g_uu[a,d]*(dg_ddd[c,d,b] + dg_ddd[b,d,c] - dg_ddd[d,b,c])
+"""
+    Christoffel_symbols(g_uu::Matrix, dg_ddd::Array{T, 3} where T)
+
+Computes the Christoffel symbols given the metric
+
+# Arguments
+- `g_uu` contravariant metric
+- `dg_ddd` array of derivatives of the metric (as returned by `metric_deriv`)
+"""
+function Christoffel_symbols(g_uu::SMatrix, dg_ddd::SArray)
+    Γ_udd = @MArray zeros(eltype(g_uu), 4, 4, 4)
+    @tullio Γ_udd[a,b,c] = 0.5*g_uu[a,d]*(dg_ddd[c,d,b] + dg_ddd[b,d,c] - dg_ddd[d,b,c])
     return Γ_udd
 end
 
 """
-Computes the Christoffel symbols given the metric function and the coordinates
+    Christoffel_symbols(G, t::Real, x::Real, y::Real, z::Real)
 
-. G must be a function returning a Matrix{Real,4} and taking t, x, y, z as input
+Computes the Christoffel symbols given the metric
+
+# Arguments
+- `G` must be a function returning a `Matrix{Real,4}` and taking 4 `Real` as input
+- `t`, `x`, `y`, and `z` are 4 `Real` specifing the spacetime point
 """
 function Christoffel_symbols(G, t::Real, x::Real, y::Real, z::Real)
     dg_ddd = metric_deriv(G, t, x, y, z)
@@ -132,27 +172,29 @@ function Christoffel_symbols(G, t::Real, x::Real, y::Real, z::Real)
 end
 
 """
-RHS for the geodesic equations (to be handed over to DifferentialEquations)
-"""
-function geodesic_equation(u::Vector, G, λ::Real)
-    du = copy(u)
-    geodesic_equation!(du, u, G, λ)
-    return du
-end
+    geodesic_equation(G)
 
-"""
-RHS for the geodesic equations (to be handed over to DifferentialEquations)
+Creates the RHS of the geodesic equation to be passed to `DifferentialEquations`
 
-This is the in place version
+# Examples
+```julia-repl
+julia> metric(t, x, y, z) = Schwartzschild_metric(t, x, y, z, M=1.0);
+julia> fun = geodesic_equation(metric);
+julia> u0 = vcat(x0_u, p0_u);
+julia> prob = ODEProblem(fun, u0, (0., 100.));
+```
 """
-function geodesic_equation!(du::Vector{T}, u::Vector{T}, G, λ::T) where T <: Real
-    x_u = u[IT:IZ]
-    p_u = u[IZ+1:end]
-    Γ_udd = Christoffel_symbols(G, x_u...)
-    dx_u = p_u
-    @tullio dp_u[a] := - Γ_udd[a,b,c]*p_u[b]*p_u[c]
-    du[IT:IZ] = dx_u[:]
-    du[IZ+1:end] = dp_u[:]
+function geodesic_equation(G)
+    function fun!(du, u, p, λ)
+        x_u = u[IT:IZ]
+        p_u = u[IZ+1:end]
+        Γ_udd = Christoffel_symbols(G, x_u...)
+        du[IT:IZ] = p_u[:]
+        dp = @MVector zeros(eltype(du), 4)
+        @tullio dp[a] = - Γ_udd[a,b,c]*p_u[b]*p_u[c]
+        du[IZ+1:end] = dp[:]
+    end
+    return fun!
 end
 
 end
