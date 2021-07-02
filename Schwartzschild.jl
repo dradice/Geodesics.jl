f5b50448154af5199a53cf57a056af46f3683fa1
David Radice <david.radice@psu.edu>
2021-07-02 12:47:38 -0400

    Improved example code

--- Schwartzschild.jl
+++ Schwartzschild.jl
@@ -5,26 +5,44 @@ using DifferentialEquations
 using Plots
 using StaticArrays
 
-# Choose metric
+## Choose graphics
+#theme(:juno)        # you need to have PlotThemes installed
+plotly()
+
+## Choose metric
 G(t,x,y,z) = Schwartzschild_metric(t,x,y,z;M=1);
 
+## Options
+# Maximum integration
+Λ_max = 1000.0
+# Maximum redshift
+Z_max = 100.0;
 # Initial position
 ρs = 0.5;
 x_u = [0., 0., 0., (2. + √3)*ρs];
-
-# Direction
+# Initial direction
 Ω = [1., 0., 0.];
 Ω /= Ω'*Ω;
 
+## Assemble initial conditions
 # Create a null vector
 e0_u, e1_u, e2_u, e3_u = make_tetrad(G, x_u...);
 p_u = e0_u + Ω[1]*e1_u + Ω[2]*e2_u + Ω[3]*e3_u;
-
-# Solve differential equation
+# Initial conditions
 u0 = MVector{8}(vcat(x_u, p_u));
-prob = ODEProblem(geodesic_equation(G), u0, (0., 100.));
-@time prob = ODEProblem(geodesic_equation(G), u0, (0., 100.));
-sol = solve(prob, Tsit5());
 
-# Plot the solution
-plot(sol, vars=(2,3,4))
+## Integrate equations
+prob = ODEProblem(geodesic_equation(G), u0, (0, Λ_max));
+integr = init(prob, Tsit5(), reltol=1e-8, abstol=1e-6)
+while true
+    step!(integr)
+    if integr.t >= Λ_max
+        break
+    end
+    if integr.u[5]/u0[5] > Z_max
+        break
+    end
+end
+
+## Plot the solution
+plot(integr.sol, vars=(2,4))
